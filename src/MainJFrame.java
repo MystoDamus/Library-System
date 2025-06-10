7984015f3f0cbc81b67003630a2dfb1be3c50f5b
Cyron James Irigan <irigancyronjames@gmail.com>
2025-06-10 20:16:31 +0800

    Main Frame

--- src/MainJFrame.java
+++ src/MainJFrame.java
@@ -169,11 +169,15 @@ public class MainJFrame extends javax.swing.JFrame {
     }//GEN-LAST:event_jButton5ActionPerformed
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        new BookJFrame().setVisible(true);
+    BookJFrame bookFrame = new BookJFrame(this); // pass this main frame
+    bookFrame.setVisible(true);
+    this.setVisible(false); 
     }//GEN-LAST:event_jButton1ActionPerformed
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
-        new StudentJFrame().setVisible(true);
+      StudentJFrame studentFrame = new StudentJFrame(this); // pass this main frame
+    studentFrame.setVisible(true);
+    this.setVisible(false); 
     }//GEN-LAST:event_jButton2ActionPerformed
 
     private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
